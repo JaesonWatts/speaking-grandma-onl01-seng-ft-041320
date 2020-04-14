b97876ed3ecc8756333ab0cf2be64762dc1e094e
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-14 05:03:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(say_what)
   if say_what == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
-  elsif say_what.upcase == true
-    'NO, NOT SINCE 1938!'
-  else
+  elsif say_what != say_what.upcase
     'HUH?! SPEAK UP, SONNY!'
+  else
+    'NO, NOT SINCE 1938!'
   end
 end
