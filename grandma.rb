48998fc899ea0896198490bb7f7cef53a34709fd
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-14 04:52:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,13 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(say_what)
+  if say_what == "I LOVE YOU GRANDMA!"
+    'I LOVE YOU TOO PUMPKIN!'
+  elsif say_what.case == upper
+    'NO, NOT SINCE 1938!'
+  else
+    'HUH?! SPEAK UP, SONNY!'
+end
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
